35390817f70a38ec16918bfbecd4d106afa69d50
Buza Dan <buzadandeveloper@gmail.com>
2025-06-01 18:11:08 +0300

    feat: added icons

---
+++ src/icons/task-manager-icon.tsx
@@ -0,0 +1,41 @@
+export const TaskManagerIcon = () => (
+  <svg
+    viewBox='0 0 24 24'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+    className='w-6 h-6 text-black dark:text-white'
+  >
+    <path
+      d='M4 12C4 7.5817 7.5817 4 12 4C16.4183 4 20 7.5817 20 12'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <line
+      x1='8'
+      y1='9'
+      x2='14'
+      y2='9'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+    />
+    <line
+      x1='8'
+      y1='12'
+      x2='12'
+      y2='12'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+    />
+    <path
+      d='M14 17L16 19L20 15'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
+);
